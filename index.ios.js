499321d599d0907c493f71c34746eeb6f44e414c
C Powers <cjpowers3@gmail.com>
2017-01-01 01:50:47 -0800

    iitial commit - get the repo ready #lit

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import MapSchedule from './src/MapSchedule';
+
+AppRegistry.registerComponent('MapSchedule', () => MapSchedule);
---
+++ src/MapSchedule.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+
+export default class MapSchedule extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Text style={{marginTop: 50}}>Howdy ho</Text>
+    )
+  }
+}
