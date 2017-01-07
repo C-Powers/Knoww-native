8b2783ff3ebe17846631930bbb84e1c9da50af73
C Powers <cjpowers3@gmail.com>
2017-01-06 23:59:41 -0800

    passing dates

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,39 +1,72 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import Calendar from 'react-native-calendar';
 
-const Schedule = () => {
-  const customStyle = {
-    currentDayCircle: {
-      backgroundColor: 'blue',
-    },
+import Calendar from 'react-native-calendar';
+import moment from 'moment';
+
+
+export default class Schedule extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDate: moment().format(),
+    };
   }
 
   addDate = (title) => Actions.dayScene({title: title});
-  //gotoSchedule = () => Actions.schedule({text: this.state.text});
 
-  return (
-    <View>
-      <Calendar
-        customStyle={customStyle}
-        style={styles.calendarSpot}
-        scrollEnabled
-      />
-      <Text onPress={() => addDate("-- pass in the day here --")}>
-        click here to add a date
-      </Text>
-    </View>
-  )
+  render() {
+    return (
+       <View style={{marginTop: 20}}>
+         <Calendar
+           scrollEnabled
+           onDateSelect={(date) => this.setState({ selectedDate: date })}
+         />
+
+         <Text>Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')}</Text>
+
+         <Text onPress={() => this.addDate(moment(this.state.selectedDate).format('MMMM DD YYYY'))}>
+         {/* this needs to be passed in for classes! */}
+           click here to add a date
+         </Text>
+       </View>
+     )
+  }
 }
 
-const styles = StyleSheet.create({
-  calendarSpot: {
-    paddingTop:50
-  }
-});
 
-export default Schedule
+// const Schedule = () => {
+//   const customStyle = {
+//     currentDayCircle: {
+//       backgroundColor: 'blue',
+//     },
+//   }
+//
+//   addDate = (title) => Actions.dayScene({title: title});
+//   //gotoSchedule = () => Actions.schedule({text: this.state.text});
+//
+//   return (
+//     <View>
+//       <Calendar
+//         customStyle={customStyle}
+//         style={styles.calendarSpot}
+//         scrollEnabled
+//       />
+//       <Text onPress={() => addDate("-- pass in the day here --")}>
+//         click here to add a date
+//       </Text>
+//     </View>
+//   )
+// }
+//
+// const styles = StyleSheet.create({
+//   calendarSpot: {
+//     paddingTop:50
+//   }
+// });
+//
+// export default Schedule
 
 
 // export default class Schedule extends Component {
