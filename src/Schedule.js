6422b82d5d4582fc343515d6765d9de9776ada9f
C Powers <cjpowers3@gmail.com>
2017-01-09 23:17:24 -0800

    realized I don't need state, getting rid of it and make a functional component again. This means that I need a db... woo! :'[

--- src/Schedule.js
+++ src/Schedule.js
@@ -5,110 +5,139 @@ import { Actions } from 'react-native-router-flux';
 import Calendar from 'react-native-calendar';
 import moment from 'moment';
 
-export default class Schedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDate: moment().format(),
-    };
-  }
-
-  //addDate = (title) => Actions.dayScene({title: title});
-  addDate = (data) => this.setState({selectedDate: data})
-
-  render() {
-    let eventDates = ['2017-01-03', '2017-01-05', '2017-01-28', '2017-01-30']
-    const customStyle = {
-        calendarContainer: {
-          backgroundColor: '#ecf0f1',
-        },
-        day: {
-          color: '#34495e',
-        },
-        weekendDayText: {
-          color: '#95a5a6',
-        },
-        currentDayCircle: {
-          backgroundColor: '#c0392b',
-        },
-        currentDayText: {
-          color: '#c0392b',
-        },
-        eventIndicator: {
-          backgroundColor: '#8e44ad',
-        },
-        hasEventDaySelectedCircle: {
-          backgroundColor: '#8e44ad',
-        },
-
-      }
-
-    const testEvent = [{date: '2017-01-04', hasEventCircle: {backgroundColor: 'powderblue'}}];
-
-    return (
-       <View style={{marginTop: 20}}>
-         <Calendar
-            customStyle={customStyle}
-            scrollEnabled
-            showEventIndicators
-            eventDates = {eventDates}
-            //onDateSelect={(date) => this.setState({ selectedDate: date })}
-            //events={testEvent}
-            onDateSelect={(date) => this.addDate(date)}
-         />
-
-         <Text>Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')}</Text>
-
-         <Text onPress={() => this.addDate(moment(this.state.selectedDate).format('MMMM DD YYYY'))}>
-         {/* this needs to be passed in for classes! */}
-           click here to add a date
-         </Text>
-       </View>
-     )
-  }
+const ScheduleData = {
+  dates: [
+    // {'2017-01-03': [{1: "dog walk"}, {2: 'human walk'}]},
+    // {'2017-01-05': [{5: "dog walk 2"}, {8: 'human walk 2'}]},
+    {day: '2017-01-03', items: [{1: "dog walk"}, {2: 'human walk'}]},
+    {day: '2017-01-05', items: [{5: "dog walk 2"}, {8: 'human walk 2'}]}
+  ]
 }
 
+const Schedule = () => {
+  const customStyle = {
+    calendarContainer: {
+      backgroundColor: '#ecf0f1',
+    },
+    day: {
+      color: '#34495e',
+    },
+    weekendDayText: {
+      color: '#95a5a6',
+    },
+    currentDayCircle: {
+      backgroundColor: '#c0392b',
+    },
+    currentDayText: {
+      color: '#c0392b',
+    },
+    eventIndicator: {
+      backgroundColor: '#8e44ad',
+    },
+    hasEventDaySelectedCircle: {
+      backgroundColor: '#8e44ad',
+    },
+  }
 
-// const Schedule = () => {
-//   const customStyle = {
-//     currentDayCircle: {
-//       backgroundColor: 'blue',
-//     },
+  addDate = (title) => Actions.dayScene({title: title});
+  //gotoSchedule = () => Actions.schedule({text: this.state.text});
+
+  return (
+    <View style={{marginTop: 20}}>
+      <Calendar
+        customStyle={customStyle}
+        scrollEnabled
+        showEventIndicators
+        eventDates = {ScheduleData.dates.map((item) => item.day)}
+        //onDateSelect={(date) => this.setState({ selectedDate: date })}
+        //events={testEvent}
+        onDateSelect={(date) => this.addDate(moment(date).format('MMMM DD YYYY'))}
+        />
+
+     {/* <Text> Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')} </Text> */}
+
+     <Text onPress={() => this.addDate(moment(this.state.selectedDate).format('MMMM DD YYYY'))}>
+       click here to add a date
+     </Text>
+   </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  calendarSpot: {
+    paddingTop:50
+  }
+});
+
+export default Schedule
+
+
+// export default class Schedule extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       dayData: [
+//         {'2017-01-03': [{1: "dog walk"}, {2: 'human walk'}]}
+//       ],
+//     };
 //   }
-//
-//   addDate = (title) => Actions.dayScene({title: title});
 //   //gotoSchedule = () => Actions.schedule({text: this.state.text});
+//   addDate = (day) => {
+//     this.dayData.push(day)
+//     Actions.dayScene({title: this.state.dayData.day})
+//     };
+//   //addDate = (data) => this.setState({selectedDate: data});
 //
-//   return (
-//     <View>
-//     <Calendar
-//        //sref="calendar"
-//        showEventIndicators
-//        eventDates={['2016-07-03', '2016-07-05', '2016-07-28', '2016-07-30']}
-//        events={[{date: '2016-07-04', hasEventCircle: {backgroundColor: 'powderblue'}}, {date: '2017-01-04', hasEventCircle: {backgroundColor: 'powderblue'}}]}
-//        scrollEnabled
-//        showControls
-//        //dayHeadings={customDayHeadings}
-//        //monthNames={customMonthNames}
-//        titleFormat={'MMMM YYYY'}
-//        prevButtonText={'Prev'}
-//        nextButtonText={'Next'}
-//       />
-//       <Text onPress={() => addDate("-- pass in the day here --")}>
-//         click here to add a date
-//       </Text>
-//     </View>
-//   )
-// }
+//   render() {
+//     let eventDates = ['2017-01-03', '2017-01-05', '2017-01-28', '2017-01-30']
+//     const customStyle = {
+//         calendarContainer: {
+//           backgroundColor: '#ecf0f1',
+//         },
+//         day: {
+//           color: '#34495e',
+//         },
+//         weekendDayText: {
+//           color: '#95a5a6',
+//         },
+//         currentDayCircle: {
+//           backgroundColor: '#c0392b',
+//         },
+//         currentDayText: {
+//           color: '#c0392b',
+//         },
+//         eventIndicator: {
+//           backgroundColor: '#8e44ad',
+//         },
+//         hasEventDaySelectedCircle: {
+//           backgroundColor: '#8e44ad',
+//         },
 //
-// const styles = StyleSheet.create({
-//   calendarSpot: {
-//     paddingTop:50
+//       }
+//
+//     const testEvent = [{date: '2017-01-04', hasEventCircle: {backgroundColor: 'powderblue'}}];
+//
+//     return (
+//        <View style={{marginTop: 20}}>
+//          <Calendar
+//             customStyle={customStyle}
+//             scrollEnabled
+//             showEventIndicators
+//             eventDates = {eventDates}
+//             //onDateSelect={(date) => this.setState({ selectedDate: date })}
+//             //events={testEvent}
+//             onDateSelect={(date) => this.addDate(moment(date).format('MMMM DD YYYY'))}
+//          />
+//
+//          <Text> Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')} </Text>
+//
+//          <Text onPress={() => this.addDate(moment(this.state.selectedDate).format('MMMM DD YYYY'))}>
+//            click here to add a date
+//          </Text>
+//        </View>
+//      )
 //   }
-// });
-//
-// export default Schedule
-
+// }
 
 // export default class Schedule extends Component {
 //
